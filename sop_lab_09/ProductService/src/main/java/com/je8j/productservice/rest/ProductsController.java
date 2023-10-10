c638a997e1a3d4e2b06e9d7461328c7151dc5da7
7E8J <103146285+7E8J@users.noreply.github.com>
2023-10-10 11:00:23 +0700

    พวกเวร

---
+++ sop_lab_09/ProductService/src/main/java/com/je8j/productservice/rest/ProductsController.java
@@ -0,0 +1,55 @@
+package com.je8j.productservice.rest;
+
+import com.je8j.productservice.command.CreateProductCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public ProductsController(Environment env, CommandGateway commandGateway) {
+        this.env = env;
+        this.commandGateway = commandGateway;
+    }
+
+    @GetMapping
+    public String getProduct(){
+        return "Http GET Handled "+ env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createProduct(@RequestBody CreateProductRestModel model){
+        CreateProductCommand command = CreateProductCommand.builder()
+                .productId(UUID.randomUUID().toString())
+                .title(model.getTitle())
+                .price(model.getPrice())
+                .quantity(model.getQuantity())
+                .build();
+        String result ;
+        try{
+            result = commandGateway.sendAndWait(command);
+        } catch(Exception e) {
+            result = e.getLocalizedMessage();
+        }
+
+        return result;
+    }
+
+    @PutMapping
+    public String updateProduct(){
+        return "Http PUT Handled";
+    }
+
+    @DeleteMapping
+    public String deleteProduct(){
+        return "Http DELETE Handled";
+    }
+}
